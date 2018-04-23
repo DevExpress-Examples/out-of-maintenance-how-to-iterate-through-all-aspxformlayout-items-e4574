215290f96ae008e957bdd52e3e0c7bd65ab37bdd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to iterate through all ASPxFormLayout items
+
+
+<p>This example demonstrates how to iterate through all ASPxFormLayout items using the <a href="http://documentation.devexpress.com/#AspNet/clsDevExpressWebASPxFormLayoutLayoutGroupBasetopic"><u>LayoutGroupBase.ForEach method</u></a>.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,40 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxFormLayout
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+	End Sub
+	Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+		For Each item In layout.Items
+			If TypeOf item Is LayoutGroupBase Then
+				TryCast(item, LayoutGroupBase).ForEach(AddressOf GetNestedControls)
+			End If
+		Next item
+	End Sub
+	Private Sub GetNestedControls(ByVal item As LayoutItemBase)
+		For Each c As LayoutItemBase In item.Collection
+			If TypeOf c Is LayoutGroup Then
+				TryCast(c, LayoutGroup).ForEach(AddressOf GetNestedControls)
+			End If
+			If TypeOf c Is LayoutItem Then
+				SetValue(TryCast(c, LayoutItem))
+			End If
+		Next c
+	End Sub
+	Private Shared Sub SetValue(ByVal c As LayoutItem)
+		For Each control As Control In c.LayoutItemNestedControlContainer.Controls
+			Dim editor As ASPxEdit = TryCast(control, ASPxEdit)
+			If editor IsNot Nothing Then
+				editor.Value = DateTime.Now.ToString()
+			End If
+		Next control
+	End Sub
+End Class
